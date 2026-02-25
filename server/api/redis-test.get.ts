1631447969109e654bb67a10c8ecd0b692a3adac
Abdullah-Elnasan <abo777hodefa@gmail.com>
2026-02-25 14:59:59 +0300

    test

--- server/api/redis-test.get.ts
+++ server/api/redis-test.get.ts
@@ -4,4 +4,6 @@ import { redis } from '../utils/redis'  // عدّل المسار حسب مشرو
 export default defineEventHandler(async () => {
   const value = await redis.ping()
   return { ok: true, value }
+
+  
 })
